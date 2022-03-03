3ea5ba9a77ecfd406b318dcd72b69b968afcd8d7
Jonathan Araque <jaap5227@gmail.com>
2022-03-03 17:54:12 -0500

    Added CurrencyConversion service

---
+++ services/currency-conversion-service/src/main/java/com/joadarpe/microservices/currencyconversionservice/proxies/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.joadarpe.microservices.currencyconversionservice.proxies;
+
+import com.joadarpe.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable("from") String from,
+            @PathVariable("to") String to);
+
+}
