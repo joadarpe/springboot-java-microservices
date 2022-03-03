6c6dab47e3a22952304fb7b86f5931ca0a833548
Jonathan Araque <jaap5227@gmail.com>
2022-03-03 16:13:50 -0500

    Added CurrencyExchange service

---
+++ services/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','COP',65,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'EUR','COP',75,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'AUD','COP',25,'');
