73a90713ef74d3f9f71d0ab61f98a793419d763e
Jonathan Araque <jaap5227@gmail.com>
2022-03-08 16:56:26 -0500

    Changing versions and adding customized environment variable from deployment.yml

--- services/currency-exchange-service/src/main/java/com/joadarpe/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ services/currency-exchange-service/src/main/java/com/joadarpe/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -40,7 +40,7 @@ public class CurrencyExchangeController {
 
         String port = environment.getProperty("local.server.port");
         String host = environment.getProperty("HOSTNAME");
-        String version = "v1";
+        String version = "v2";
         currencyExchange.setEnvironment(String.format("%s %s %s", port, host, version));
 
         return currencyExchange;
